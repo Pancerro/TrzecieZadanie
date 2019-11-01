aba2656dee1e815207102cbe2f2ffc0bb3b806b1
Adrian Ławecki <pancerro@gmail.com>
2019-11-01 20:12:53 +0100

    Blog app for studies

---
+++ blog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pance\onedrive\pulpit\python\blog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mojblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MojblogConfig(AppConfig):
+    name = 'mojblog'
